1f46e62f7adb4200ce6e90f0719d57eacf17901f
photoday0914 <95325234+photoday0914@users.noreply.github.com>
2022-02-06 23:23:58 -0800

    initial upload

---
+++ submit/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">    
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
